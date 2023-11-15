5a4788b5715382d51bfb15cff8cd7f395019f8b7
bokhtiar <bokhtiar@gmail.com>
2023-11-16 00:19:09 +0600

    product crated

--- src/pages/product/create.jsx
+++ src/pages/product/create.jsx
@@ -3,27 +3,34 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Toastify } from "../../components/toastify"
 import { NetworkServices } from '../../network/index'
+import { CKEditor } from '@ckeditor/ckeditor5-react';
 import { PrimaryButton } from "../../components/button";
 import { BreadCrumbs } from "../../components/breadCrumbs";
 import { networkErrorHandeller } from "../../utils/helper";
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { SingleSelect, TextInput } from "../../components/input";
+
 
 export const ProductCreate = () => {
     const navigate = useNavigate()
     const [file, setFile] = useState()
-    const [options, setOptions] = useState([]);
+    const [options, setOptions] = useState([])
+    const [plantBody, setPlantBody] = useState()
     const [loading, setLoading] = useState(false)
     const [selectedFile, setSelectedFile] = useState();
-    const [fields, setFields] = useState([{ size: 'm', price: '3' }]);
+    const [fields, setFields] = useState([{ size: '', price: '' }]);
 
     /** file submit */
     function handleChangeFile(event) {
         setFile(event.target.files[0])
     }
 
+    console.log("gt", plantBody);
+
     const {
         control,
         handleSubmit,
+        register,
         formState: { errors },
     } = useForm()
 
@@ -44,7 +51,7 @@ export const ProductCreate = () => {
     };
 
     const onSubmit = async (data) => {
-        // setLoading(true)
+        setLoading(true)
         const formData = new FormData();
         formData.append("image", file);
         formData.append("size", JSON.stringify(fields));
@@ -52,12 +59,10 @@ export const ProductCreate = () => {
         formData.append("title", data?.title);
         formData.append("price", data?.price);
         formData.append("ratting", data?.ratting);
-        formData.append("plant_body", data?.plant_body);
+        formData.append("plant_body", plantBody);
         formData.append("category_id", data?.category_id?.value);
 
         try {
-            console.log("data", formData);
-            console.log("field", fields);
             const response = await NetworkServices.Product.store(formData)
             navigate('/dashboard/product')
             return Toastify.Success(response.data.message);
@@ -153,7 +158,48 @@ export const ProductCreate = () => {
                         placeholder="Select category"
                     />
 
-                    {/* product title */}
+                    <div>
+                        {fields.map((field, index) => (
+                            <>
+                                <p className="text-sm mb-1 text-gray-500">Size with price</p>
+                                <div key={index} className="field-wrapper grid grid-cols-3 gap-3">
+
+                                    <input
+                                        type="text"
+                                        placeholder="Size"
+                                        value={field.size}
+                                        className="w-full border border-gray-200 rounded-md p-3"
+                                        onChange={(e) => handleChange(index, 'size', e.target.value)}
+                                    />
+                                    <input
+                                        type="number"
+                                        placeholder="price"
+                                        value={field.price}
+                                        className="w-full border border-gray-200 rounded-md p-3"
+                                        onChange={(e) => handleChange(index, 'price', e.target.value)}
+                                    />
+                                    <div className=" gap-3 my-auto">
+                                        <span onClick={addField} class="border border-green-500 rounded-full material-symbols-outlined p-1">
+                                            add
+                                        </span>
+                                        <span onClick={() => removeField(index)} class=" ml-1 border border-green-500 rounded-full material-symbols-outlined p-1">
+                                            remove
+                                        </span>
+                                    </div>
+                                </div>
+                            </>
+                        ))}
+                    </div>
+
+
+                </div>
+
+
+
+
+                {/* product title */}
+
+                <div className="my-3">
                     <TextInput
                         label="Product body"
                         name="body"
@@ -163,44 +209,37 @@ export const ProductCreate = () => {
                         error={errors.body && errors.body.message}
                         rules={{ required: "Product body is required" }}
                     />
+                </div>
 
-                    {/* product title */}
-                    <TextInput
-                        label="Product plant body"
-                        name="plant_body"
-                        type="text"
-                        placeholder="Enter product plant body"
-                        control={control}
-                        error={errors.plant_body && errors.plant_body.message}
-                        rules={{ required: "Product plant body is required" }}
+
+                <div className="mt-5">
+                    <p className="text-sm mb-1 text-gray-500">Plant Body</p>
+                    <CKEditor
+                        {...register("plant_body")}
+                        editor={ClassicEditor}
+                        data="<p>Hello from CKEditor&nbsp;5!</p>"
+                        onReady={editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log('Editor is ready to use!', editor);
+                        }}
+                        onChange={(event, editor) => {
+                            const data = editor.getData();
+                            console.log("body", data);
+                            // console.log({ event, editor, data });
+                            setPlantBody(data)
+                        }}
+                        onBlur={(event, editor) => {
+                            // console.log('Blur.', editor);
+                        }}
+                        onFocus={(event, editor) => {
+                            // console.log('Focus.', editor);
+                        }}
                     />
                 </div>
-                {fields.map((field, index) => (
-                    <div key={index} className="field-wrapper grid grid-cols-3 mt-5 gap-3">
-                        <input
-                            type="text"
-                            placeholder="Size"
-                            value={field.size}
-                            className="w-full border border-gray-200 rounded-md px-3"
-                            onChange={(e) => handleChange(index, 'size', e.target.value)}
-                        />
-                        <input
-                            type="number"
-                            placeholder="price"
-                            value={field.price}
-                            className="w-full border border-gray-200 rounded-md px-3"
-                            onChange={(e) => handleChange(index, 'price', e.target.value)}
-                        />
-                        <div className="">
-                            <span onClick={addField} class="border border-green-500 rounded-full material-symbols-outlined p-1">
-                                add
-                            </span>
-                            <span onClick={() => removeField(index)} class="border border-green-500 rounded-full material-symbols-outlined p-1">
-                                remove
-                            </span>
-                        </div>
-                    </div>
-                ))}
+
+
+
+
 
 
 
